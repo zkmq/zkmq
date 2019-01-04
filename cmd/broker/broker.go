edb4c24aa813aae5e27bcc53cdbf7747b78523fa
yayanyang <org.lemon@gmail.com>
2019-01-04 17:05:34 +0800

    fix bug

--- cmd/broker/broker.go
+++ cmd/broker/broker.go
@@ -7,6 +7,7 @@ import (
 	"github.com/dynamicgo/gomesh/app"
 	_ "github.com/dynamicgo/slf4go-aliyun"
 	"github.com/zkmq/zkmq/services/broker"
+	"github.com/zkmq/zkmq/services/metadata"
 	"github.com/zkmq/zkmq/services/storage"
 )
 
@@ -17,6 +18,10 @@ func main() {
 
 	gomesh.LocalService("zkmq.Storage", func(config config.Config) (gomesh.Service, error) {
 		return storage.New(config)
+	})
+
+	gomesh.LocalService("zkmq.Metadata", func(config config.Config) (gomesh.Service, error) {
+		return metadata.New(config)
 	})
 
 	app.Run("zkmq-broker")
